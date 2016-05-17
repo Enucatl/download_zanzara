28646a7fadf45de7f70494c606df575a7acd2097
Matteo Abis <gmatteo..abis@gmail.com>
2016-05-17 14:02:29 +0200

    add logging and verbose option

--- download_zanzara.py
+++ download_zanzara.py
@@ -3,6 +3,11 @@ import os
 import click
 import requests
 import pandas as pd
+import logging
+
+
+def progress_bar(fraction):
+    return '[{0:50s}] {1:.1%}'.format('#' * int((fraction * 50)), fraction)
 
 
 @click.command()
@@ -14,7 +19,12 @@ import pandas as pd
     "--end_date",
     default="20160220",
     help="end date (yyyymmdd)")
-def main(start_date, end_date):
+@click.option(
+    "-v",
+    "--verbose",
+    count=True,
+    )
+def main(start_date, end_date, verbose):
     """download de la zanzara, date in formato yyyymmdd
 
     :start_date: string yyyymmdd
@@ -22,18 +32,31 @@ def main(start_date, end_date):
     :returns: nothing, saves mp3 files
 
     """
+    levels = [
+        logging.WARNING,
+        logging.DEBUG,
+    ]
+    log_format = '%(asctime)s - %(name)s - %(message)s'
+    logger = logging.getLogger(__name__)
+    verbose = min(len(levels) - 1, verbose)
+    logging.basicConfig(level=levels[verbose], format=log_format)
     start_date = datetime.datetime.strptime(start_date, "%Y%m%d")
     end_date = datetime.datetime.strptime(end_date, "%Y%m%d")
     daterange = pd.date_range(start_date, end_date)
-    for day in daterange:
+    n = len(daterange)
+    for i, day in enumerate(daterange):
         url = "http://audio.radio24.ilsole24ore.com/radio24_audio/{}/{}-lazanzara.mp3".format(
             day.strftime("%Y"), day.strftime("%y%m%d"))
         response = requests.get(url, stream=True)
-        print(url, response)
+        logger.debug("requested url %s", url)
+        logger.debug("got %s", response)
+        logger.warning(progress_bar((i + 1) / n))
         if response.ok:
+            output_path = os.path.basename(url)
             with open(os.path.basename(url), "wb") as output_file:
                 for chunk in response.iter_content(1024):
                     output_file.write(chunk)
+            logger.debug("written %s", output_path)
 
 
 if __name__ == "__main__":
